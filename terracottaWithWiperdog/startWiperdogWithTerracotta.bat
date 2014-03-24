b309ee00baeb5d7ec8881133d8cc284ff17fd528
nghia <nghia@nghia-virtual-machine.(none)>
2014-03-24 17:08:29 +0700

    update startWiperdogWithTerracotta.bat

--- terracottaWithWiperdog/startWiperdogWithTerracotta.bat
+++ terracottaWithWiperdog/startWiperdogWithTerracotta.bat
@@ -1,35 +1,28 @@
-self="$0"
-while [ -h "$self" ]; do
-	res=`ls -ld "$self"`
-	ref=`expr "$res" : '.*-> \(.*\)$'`
-	if expr "$ref" : '/.*' > /dev/null; then
-		self="$ref"
-	else
-		self="`dirname \"$self\"`/$ref"
-	fi
-done
+@echo off
+cls
+SET CUR_DIR=%CD%
 
-dir=`dirname "$self"`
-CUR_DIR=`cd "$dir/" && pwd`
+:installDefault
+    SET uchoice=
+    SET /P uchoice=You did not specify any option, would you like to install with default option[Yy(default)/Other key to exit]?
+    if "%uchoice%" == "" goto:eof
+    if not "%uchoice%" == "y" (
+        echo NOT y
+        if not "%uchoice%" == "Y" (
+            echo NOT Y
+            goto:eof
+        )
+    )    
+    pause
+    call:usage        
+    call %CUR_DIR%\getWiperdog.bat
+    call %CUR_DIR%\installWiperdog.bat
+    call %CUR_DIR%\configure.bat
+    call %CUR_DIR%\configureWithJobManager.bat
+    call %CUR_DIR%\runWiperdog.bat
+goto:eof
 
-function installDefault
-(
-    echo "You did not choose any option, would you like to install with default option?[y=accept/any key=exit]"
-    read uchoice
-    if [ -n $uchoice  ] && [ ! $uchoice == "y" ]
-    then
-        usage
-        exit 0
-    fi 
-    . ./getWiperdog.sh
-    . ./installWiperdog.sh
-    . ./configure.sh
-    . ./configureWithJobManager.sh
-    . ./runWiperdog.sh
-)
-
-function usage
-(
+:usage
     echo This is a reproduce wiperdog with terracotta script.
 	echo startWiperdogWithTerracotta.bat [options]
 	echo options : 
@@ -38,67 +31,68 @@ function usage
 	echo - /wjm : Use JobManager bundle in the wiperdog [If not, use JobManger embered in Groovy script]
 	echo - /rw : Run wiperdog
 	echo - /h : Open help
+goto:eof
+set argC=0
+for %%x in (%*) do Set /A argC+=1
+
+if %argC% == 0 (  
+    call:installDefault
+	goto:eof
 )
 
-if [ $# -eq 0 ]
-  then
-    installDefault
-	exit
-fi
-
 # Default value of variables
-GET_WIPERDOG="FALSE"
-INSTALL_WIPERDOG="FALSE"
-WITH_JOB_MANAGER="FALSE"
-RUN_WIPERDOG="FALSE"
+SET GET_WIPERDOG="FALSE"
+SET INSTALL_WIPERDOG="FALSE"
+SET WITH_JOB_MANAGER="FALSE"
+SET RUN_WIPERDOG="FALSE"
 
 # Get input parameters
-while [ "$1" != "" ]; do
-	case $1 in 
-		/gw) GET_WIPERDOG="TRUE"
-		;;
-		/iw) INSTALL_WIPERDOG="TRUE"
-		;;
-		/rw) RUN_WIPERDOG="TRUE"
-		;;
-		/wjm) WITH_JOB_MANAGER="TRUE"
-		;;
-		/h | /help)	usage
-				exit
-		;;
-		* )	usage
-			exit 1
-	esac
-	shift
-done
+for %%x in (%*) do (
+  if "%%x" == "/gw" (
+  	SET GET_WIPERDOG="TRUE"
+  )
+  if "%%x" == "/iw" (
+  	SET INSTALL_WIPERDOG="TRUE"
+  )
+  if "%%x" == "/rw" (
+  	SET RUN_WIPERDOG="TRUE"
+  )
+  if "%%x" == "/wjm" (
+  	SET WITH_JOB_MANAGER="TRUE"
+  )
+  if "%%x" == "/h" (
+  	SET GET_WIPERDOG="TRUE"
+  	call:usage
+  )
+)
 
-if [ ! -d quartz-2.2.1 ];then 
-  . ./checkoutQuartz.sh
-fi
+if not exist quartz-2.2.1 (
+  call %CUR_DIR%\checkoutQuartz.bat
+)
 
-cd $CUR_DIR
+cd %CUR_DIR%
 
 # GET WIPERDOG FROM MAVEN BY mvn COMMAND
-if [ $GET_WIPERDOG = "TRUE" ]; then
-  . ./getWiperdog.sh
-fi 
+if "%GET_WIPERDOG%" == "TRUE" (
+  call %CUR_DIR%\getWiperdog.bat
+)
 
 # INSTALL WIPERDOG
-if [ $INSTALL_WIPERDOG = "TRUE" ]; then
-  . ./installWiperdog.sh
-fi
+if "%INSTALL_WIPERDOG%" == "TRUE" (
+  call %CUR_DIR%\installWiperdog.bat
+)
 
 # INSTALL WIPERDOG WITHOUT JOB MANAGER
-if [ $WITH_JOB_MANAGER = "FALSE" ]; then
-  . ./configure.sh
-fi
+if "%WITH_JOB_MANAGER%" == "FALSE" (
+  call %CUR_DIR%\configure.bat
+)
 
 # INSTALL WIPERDOG WITH JOB MANAGER
-if [ $WITH_JOB_MANAGER = "TRUE" ]; then
-  . ./configureWithJobManager.sh
-fi
+if "%WITH_JOB_MANAGER%" = "TRUE" (
+  %CUR_DIR%\configureWithJobManager.bat
+)
 
 # START WIPERDOG
-if [ $RUN_WIPERDOG = "TRUE" ]; then
-. ./runWiperdog.sh
-fi
+if "%RUN_WIPERDOG%" = "TRUE" (
+	%CUR_DIR%\runWiperdog.bat
+)
